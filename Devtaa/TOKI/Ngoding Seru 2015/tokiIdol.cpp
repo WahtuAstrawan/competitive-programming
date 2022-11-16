53ec1bf304ebf74c0f375ca927d1671d97fb055d
Devtaaa <113565766+devtadanarsa@users.noreply.github.com>
2022-11-16 15:55:55 +0800

    Create tokiIdol.cpp

---
+++ Devtaa/TOKI/Ngoding Seru 2015/tokiIdol.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    short juri1, juri2, juri3;
+    cin >> juri1 >> juri2 >> juri3;
+    if((juri1 + juri2 + juri3) > 200){
+        cout << "Lolos";
+    }else{
+        cout << "Tidak Lolos";
+    }
+
+    return 0;
+}
