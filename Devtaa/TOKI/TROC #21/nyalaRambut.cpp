afcb709a26c9259a9edc017f54dc4b2da8da5c21
Devtaaa <113565766+devtadanarsa@users.noreply.github.com>
2022-11-16 15:50:33 +0800

    Create nyalaRambut.cpp

---
+++ Devtaa/TOKI/TROC #21/nyalaRambut.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int apiMuncul, derajatSuhu;
+    cin >> apiMuncul >> derajatSuhu;
+    if(derajatSuhu == 1){
+        cout << "NO";
+    }else{
+        cout << "YES";
+    }
+
+    return 0;
+}
