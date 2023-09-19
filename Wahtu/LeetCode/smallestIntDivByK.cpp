851580e06f6c3bda671d6021f22953e866ccd31d
Wahtu <112930889+WahtuAstrawan@users.noreply.github.com>
2023-09-19 08:03:51 +0800

    Create smallestIntDivByK.cpp

---
+++ Wahtu/LeetCode/smallestIntDivByK.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        for(int i = 0, n = 1; n <= k; n++){
+            if((i = (i * 10 + 1) % k) == 0) return n;
+        }
+        return -1;
+    }
+};
