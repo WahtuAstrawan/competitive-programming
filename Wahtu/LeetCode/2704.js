1f57b0203de0a80f40a7b4fa82ea5a395b47abaa
Wahtu <112930889+WahtuAstrawan@users.noreply.github.com>
2024-01-17 10:16:46 +0800

    Create 2704.js

---
+++ Wahtu/LeetCode/2704.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+
+    return {
+        toBe : (x) => {
+            if(val !== x){
+                throw new Error("Not Equal")
+            }
+            return true
+        },
+
+        notToBe : (x) => {
+            if(val !== x){
+                return true
+            }
+            throw new Error("Equal")
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
