f702c512bbdc5ab36c2e848c7430e64969ba5638
Wahtu <112930889+WahtuAstrawan@users.noreply.github.com>
2024-02-22 11:45:21 +0800

    Create 997.go

---
+++ Wahtu/LeetCode/997.go
@@ -0,0 +1,25 @@
+func findJudge(n int, trust [][]int) int {
+    if 1 == n {
+        return 1
+    }
+    
+    trustScore := make( []int, n+1)
+    
+    for _, trustVector := range trust{
+        
+        p1 := trustVector[0]
+        p2 := trustVector[1]
+        
+        trustScore[ p1 ] -= 1
+        
+        trustScore[ p2 ] += 1
+    }
+    
+    for personID := 1 ; personID <= n ; personID++{
+        if ( n - 1 ) == trustScore[ personID ]{
+            return personID
+        }
+    }
+    
+    return -1
+}
