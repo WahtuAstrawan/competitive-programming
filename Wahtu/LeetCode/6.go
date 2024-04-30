b32e3b31e1fb368807bfc0d8dd32ab640ed1a3bb
Wahtu <112930889+WahtuAstrawan@users.noreply.github.com>
2024-05-01 07:24:39 +0800

    Create 6.go

---
+++ Wahtu/LeetCode/6.go
@@ -0,0 +1,29 @@
+func convert(s string, numRows int) string {
+	if numRows <= 1 {
+		return s
+	}
+
+	colStep := numRows + numRows - 2
+
+	ss := make([]uint8, len(s), len(s))
+
+	diagStep := colStep - 2
+
+	i := 0
+	for row := 0; row < numRows; row = row + 1 {
+		diag := row > 0 && row < numRows-1
+		for j := row; j < len(ss); j += colStep {
+			ss[i] = s[j]
+			i += 1
+			if diag && j+diagStep < len(s) {
+				ss[i] = s[j+diagStep]
+				i += 1
+			}
+		}
+		if diag {
+			diagStep -= 2
+		}
+	}
+
+	return string(ss)
+}
