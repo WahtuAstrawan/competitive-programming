6f44e0ce09e74c465a965b17d1fbaf0e11aab8f2
Wahtu <112930889+WahtuAstrawan@users.noreply.github.com>
2026-01-30 19:32:03 +0800

    Create 1929.go

---
+++ Wahtu/LeetCode/1929.go
@@ -0,0 +1,10 @@
+func getConcatenation(nums []int) []int {
+    ans := make([]int, len(nums) * 2)
+
+    for i := 0; i < len(nums); i++ {
+        ans[i] = nums[i]
+        ans[len(nums) + i] = nums[i]
+    }
+
+    return ans
+}
